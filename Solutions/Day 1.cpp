7d75de1a466a0e832429d98df1f08f5188292801
Jaeseok <Jaeseok.cho@outlook.com>
2020-12-01 17:24:48 +0000

    Day 1 Solution

--- Solutions/Day 1.cpp
+++ Solutions/Day 1.cpp
@@ -1,10 +1,41 @@
-//
-// Created by Jaeseok on 01/12/2020.
-//
 
 #include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+const int Size = 200;
+
+int findPair(int array[], int head, int tail){
+
+    int total = array[head] + array[tail];
+    if (total == 2020){
+        return array[head] * array[tail];
+    }
+    else if (total > 2020){
+        findPair(array, head, tail - 1);
+    }
+    else {
+        findPair(array, head + 1, tail);
+    }
+}
+
+int main(){
+
+    ifstream file("Inputs/Day1");
+
+    if (!file){
+        cout << "Error";
+    }
+    if (file.is_open()) {
+        int myArray[Size];
+
+        for (int i = 0; i < Size; ++i) {
+            file >> myArray[i];
+        }
+        sort(myArray, myArray + Size);
+        cout << findPair(myArray, 0, 199);
+    }
     return 0;
 }
+
