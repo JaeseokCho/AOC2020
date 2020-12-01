479a3f79c4bd8c3e92ce27bef83855fc2f56aeff
Jaeseok <Jaeseok.cho@outlook.com>
2020-12-01 17:50:26 +0000

    Day 1 Solution

--- Solutions/Day 1.cpp
+++ Solutions/Day 1.cpp
@@ -1,11 +1,11 @@
-
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <bits/stdc++.h>
 using namespace std;
 
 const int Size = 200;
-
+// Solution to part A
 int findPair(int array[], int head, int tail){
 
     int total = array[head] + array[tail];
@@ -20,6 +20,19 @@ int findPair(int array[], int head, int tail){
     }
 }
 
+// Solution to part B
+int findTriplets(int array[], int arraySize){
+    for (int i = 0; i < arraySize - 2; ++i){
+        unordered_set<int> set;
+        int currentSum = 2020 - array[i];
+        for (int j = i + 1; j < arraySize; ++j){
+            if (set.find(currentSum - array[j]) != set.end()){
+                return array[i] * array[j] * (currentSum - array[j]);
+            }
+            set.insert(array[j]);
+        }
+    }
+}
 int main(){
 
     ifstream file("Inputs/Day1");
@@ -34,7 +47,8 @@ int main(){
             file >> myArray[i];
         }
         sort(myArray, myArray + Size);
-        cout << findPair(myArray, 0, 199);
+        cout << "The solution to part A is: " << findPair(myArray, 0, 199 ) << endl;
+        cout << "The solution to part B is: " << findTriplets(myArray, Size);
     }
     return 0;
 }
