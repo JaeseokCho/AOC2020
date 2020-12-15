0e8ae151c65d0177035375c5c6004709c7bf8a5e
Jaeseok <Jaeseok.cho@outlook.com>
2020-12-15 15:54:16 +0000

    Day 3 completed, changed to create a trees function to adjust to different values for part B and adjusted the code to accommodate for part A

--- Solutions/Day 3.cpp
+++ Solutions/Day 3.cpp
@@ -2,20 +2,40 @@
 
 using namespace std;
 
-int main() {
-    ifstream file("Inputs/Day3");
+int trees(int right, int down, vector<string> forest){
 
-    string line;
-    int count = 0, tree = 0, check = 0;
-
-    while (getline(file, line)){
-        check = (count*3) % 31;
-        if (line[check] == '#'){
-            tree++;
+    int x = 0, y = 0;
+    long long tree = 0;
+    int rows = forest.size();
+    int columns = forest[0].size();
+    while (y < rows){
+        if (forest[y][x] == '#'){
+            ++tree;
         }
-
-        count += 1;
+        x = (x + right) % columns;
+        y += down;
     }
 
-    cout << "The solution to part A is : " << tree << endl;
+    return tree;
+}
+
+int main() {
+    ifstream file("Inputs/Day3");
+    vector<string> forest;
+    string line;
+    while(getline(file, line)){
+        forest.push_back(line);
+    }
+    int answer = 0;
+    answer = trees(3, 1, forest);
+    cout << "The solution to part A is : " << answer << endl;
+
+    answer *= trees(1, 1, forest);
+    answer *= trees(5, 1, forest);
+    answer *= trees(7, 1, forest);
+    answer *= trees(1, 2, forest);
+
+    cout << "The solution to part B is : " << answer << endl;
+
+    return 0;
 }
